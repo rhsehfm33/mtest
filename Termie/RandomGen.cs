0e6760647e093a3c1b08e1e3a538d1d92f36fde4
rhsehfm33 <rhsehfm33@gmail.com>
2018-12-04 16:53:57 +0900

    Merge RandomGenerate

---
+++ Termie/RandomGen.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Ports;
+using System.Collections;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Termie
+{
+    public sealed class RandomGen
+    {
+        Thread _genThread;
+        volatile bool _bIsGenerating;
+        volatile bool _bKeepAlive;
+
+        //begin Singleton pattern
+        static readonly RandomGen instance = new RandomGen();
+
+        static RandomGen()
+        {
+        }
+
+        RandomGen()
+        {
+            _bIsGenerating = false;
+        }
+
+        public static RandomGen Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public bool IsGenerating
+        {
+            get
+            {
+                return _bIsGenerating;
+            }
+        }
+        public void ChangeGenStat()
+        {
+            _bIsGenerating = !_bIsGenerating;
+        }
+
+        public void CreateThread()
+        {
+            _bKeepAlive = true;
+            _genThread = new Thread(RandomGenerate);
+            _genThread.Start();
+        }
+        private void RandomGenerate()
+        {
+            // 스톱워치로 초당 5번 or Sleep함수로 강제 스탑? or 쓰레드 대기?
+            Stopwatch sw = new Stopwatch();
+            sw.Reset();
+            sw.Start();
+
+            CommPort com = CommPort.Instance;
+
+            string strRandomString = "";
+
+            Random rRand = new Random();
+
+            try
+            {
+                while (_bKeepAlive)
+                {
+                    if (_bIsGenerating &&sw.ElapsedMilliseconds > 200.0F)
+                    {
+                        sw.Restart();
+                        strRandomString = 'e' + rRand.Next(0, 1000).ToString() + Token.seriesToken + rRand.Next(0, 1000).ToString() + Token.seriesToken + Token.lineToken;
+                        com.Send(strRandomString);
+                    }
+                    else
+                    {
+                        TimeSpan waitTime = new TimeSpan(0, 0, 0, 0, 50);
+                        Thread.Sleep(waitTime);
+                    }
+                }
+            }
+            catch (TimeoutException) { }
+        }
+
+
+        public void Close()
+        {
+            if (_bIsGenerating || _genThread.IsAlive)
+            {
+                _bKeepAlive = false;
+                _bIsGenerating = false;
+                _genThread.Join();
+                _genThread = null;
+            }
+        }
+
+    }
+}
---
+++ Termie/새 폴더/Form3.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Termie
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graph
+            // 
+            this.graph.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.graph.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisX2.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisY.MajorGrid.Interval = 0D;
+            chartArea1.AxisY.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY2.MajorGrid.Interval = 0D;
+            chartArea1.AxisY2.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.BackColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.graph.ChartAreas.Add(chartArea1);
+            this.graph.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            legend1.Name = "Legend1";
+            legend1.Position.Auto = false;
+            legend1.Position.Height = 7.5F;
+            legend1.Position.Width = 9.90991F;
+            legend1.Position.X = 85F;
+            legend1.Position.Y = 2F;
+            this.graph.Legends.Add(legend1);
+            this.graph.Location = new System.Drawing.Point(0, 0);
+            this.graph.Name = "graph";
+            this.graph.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.graph.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Red,
+        System.Drawing.Color.Blue};
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.MarkerColor = System.Drawing.Color.Red;
+            series1.MarkerSize = 8;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series1.Name = "line1";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.MarkerColor = System.Drawing.Color.Blue;
+            series2.MarkerSize = 8;
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series2.Name = "line2";
+            this.graph.Series.Add(series1);
+            this.graph.Series.Add(series2);
+            this.graph.Size = new System.Drawing.Size(889, 534);
+            this.graph.TabIndex = 0;
+            this.graph.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            title1.Name = "Graph";
+            title1.Text = "Graph";
+            this.graph.Titles.Add(title1);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 534);
+            this.Controls.Add(this.graph);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart graph;
+    }
+}
